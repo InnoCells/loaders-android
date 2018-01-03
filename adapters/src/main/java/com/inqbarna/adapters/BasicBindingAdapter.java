65d6642b51bed85b790675cdecfb5ac078ea4de5
David García <david.garcia@inqbarna.com>
2018-01-03 16:37:36 +0100

    First attempt, not really working yet

--- adapters/src/main/java/com/inqbarna/adapters/BasicBindingAdapter.java
+++ adapters/src/main/java/com/inqbarna/adapters/BasicBindingAdapter.java
@@ -1,9 +1,27 @@
 package com.inqbarna.adapters;
 
+import android.support.annotation.NonNull;
+
+import com.google.common.base.Equivalence;
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
+import com.google.common.collect.TreeRangeSet;
 import com.inqbarna.common.AdapterSyncList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+
+import timber.log.Timber;
 
 /**
  * @author David García <david.garcia@inqbarna.com>
@@ -13,6 +31,7 @@ import java.util.List;
 public class BasicBindingAdapter<T extends TypeMarker> extends BindingAdapter {
     public static final int INVALID_IDX = -1;
     private List<T> mData;
+    private Equivalence<? super T> equivalence;
 
     protected BasicBindingAdapter() {
         this(null);
@@ -21,6 +40,11 @@ public class BasicBindingAdapter<T extends TypeMarker> extends BindingAdapter {
     public BasicBindingAdapter(ItemBinder binder) {
         setItemBinder(binder);
         mData = new ArrayList<>();
+        equivalence = Equivalence.identity();
+    }
+
+    public void setEquivalence(Equivalence<? super T> equivalence) {
+        this.equivalence = equivalence;
     }
 
     public void setItems(List<? extends T> items) {
@@ -31,7 +55,91 @@ public class BasicBindingAdapter<T extends TypeMarker> extends BindingAdapter {
         if (null != items) {
             mData.addAll(items);
         }
+        Timber.d("[SET ITEMS] Notify dataSetChanged");
         notifyDataSetChanged();
+    }
+
+    public void updateItems(@NonNull List<? extends T> items) {
+
+        final Set<Equivalence.Wrapper<? super T>> originalItemSet = toSet(mData);
+        final Set<Equivalence.Wrapper<? super T>> newItemsSet = toSet(items);
+
+
+
+        final SortedMap<Integer, T> originalItems = indexItems(mData);
+        final SortedMap<Integer, T> newItems = indexItems(items);
+        final MapDifference<Integer, T> difference = Maps.difference(originalItems, newItems, equivalence);
+        if (difference.areEqual()) {
+            return;
+        }
+
+        final DiscreteDomain<Integer> domain = DiscreteDomain.integers();
+        final Map<Integer, MapDifference.ValueDifference<T>> differing = difference.entriesDiffering();
+        final Map<Integer, T> leftOnly = difference.entriesOnlyOnLeft();
+        final Map<Integer, T> rightOnly = difference.entriesOnlyOnRight();
+
+        final RangeSet<Integer> removedIndexes = keyRanges(leftOnly, domain);
+        final RangeSet<Integer> differingIndexes = keyRanges(differing, domain);
+        final RangeSet<Integer> addedIndexes = keyRanges(rightOnly, domain);
+
+        for (T anItem : mData) {
+            onRemovingElement(anItem);
+        }
+
+        mData.clear();
+        mData.addAll(items);
+
+        for (Range<Integer> removed : removedIndexes.asRanges()) {
+            final ContiguousSet<Integer> indexes = ContiguousSet.create(removed, domain);
+            Timber.d("Indexes removed: " + indexes);
+            notifyItemRangeRemoved(addOffsets(indexes.first()), indexes.size());
+        }
+
+        for (Range<Integer> changed : differingIndexes.asRanges()) {
+            final ContiguousSet<Integer> indexes = ContiguousSet.create(changed, domain);
+            Timber.d("Indexes changed: " + indexes);
+            notifyItemRangeRemoved(addOffsets(indexes.first()), indexes.size());
+        }
+
+        for (Range<Integer> added : addedIndexes.asRanges()) {
+            final ContiguousSet<Integer> indexes = ContiguousSet.create(added, domain);
+            Timber.d("Indexes added: " + indexes);
+            notifyItemRangeInserted(addOffsets(indexes.first()), indexes.size());
+        }
+    }
+
+    protected Set<Equivalence.Wrapper<? super T>> toSet(@NonNull List<? extends T> data) {
+        final ImmutableSet.Builder<Equivalence.Wrapper<? super T>> builder = ImmutableSet.builder();
+        for (T item : data) {
+            builder.add(equivalence.wrap(item));
+        }
+        return builder.build();
+    }
+
+    @NonNull
+    private RangeSet<Integer> keyRanges(Map<Integer, ?> map, DiscreteDomain<Integer> domain) {
+        final RangeSet<Integer> indexes = TreeRangeSet.create();
+        for (Integer key : map.keySet()) {
+            indexes.add(Range.singleton(key).canonical(domain));
+        }
+        return indexes;
+    }
+
+    protected int addOffsets(int relativePos) {
+        return relativePos;
+    }
+
+    protected int removeOffsets(int absPos) {
+        return absPos;
+    }
+
+    protected SortedMap<Integer, T> indexItems(@NonNull List<? extends T> data) {
+        final ImmutableSortedMap.Builder<Integer, T> builder = ImmutableSortedMap.naturalOrder();
+        final ListIterator<? extends T> listIterator = data.listIterator();
+        while (listIterator.hasNext()) {
+            builder.put(listIterator.nextIndex(), listIterator.next());
+        }
+        return builder.build();
     }
 
     protected void onRemovingElement(T item) {
